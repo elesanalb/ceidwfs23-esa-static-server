3bbca5b50bd4409002c1df2914a9e9eabaf569cc
elena <elesanalb@gmail.com>
2024-07-02 13:53:38 +0200

    vers-4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {createServer} = require("http");
 
 const {createReadStream} = require("fs");
 
+
 function contentType(extension){
     if(extension == "html") return "text/html";
     if(extension == "css") return "text/css";
@@ -10,12 +11,28 @@ function contentType(extension){
     if(extension == "jpg" || extension == "jpeg") return "image/jpeg";
     if(extension == "png") return "image/png";
     if(extension == "json") return "application/json";
+    return "text/plain";
 }
 
+
+function servirFichero(respuesta, ruta, tipo, status){
+    respuesta.writeHead(status, {"Content-type" : tipo});
+    
+    let fichero = createReadStream(ruta);
+
+    fichero.pipe(respuesta);
+
+    fichero.on("end", () => {
+        respuesta.end();
+    });
+}
+
+
 const servidor = createServer((peticion, respuesta) => {
-    respuesta.writeHead(200, {"Content-type" : "text/html"});
+    /*respuesta.writeHead(200, {"Content-type" : "text/html"});
     respuesta.write("algo");
-    respuesta.end();
+    respuesta.end();*/
+    servirFichero(respuesta, "./publico/index.html", contentType("html"), 200);
 });
 
 servidor.listen(process.env.PORT || 3000);
