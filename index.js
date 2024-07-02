2435eb7d7f09cd5cf23d7fe2708b3fde122e8e65
elena <elesanalb@gmail.com>
2024-07-02 13:42:37 +0200

    vers-3

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const {createServer} = require("http");
 
 const {createReadStream} = require("fs");
 
+function contentType(extension){
+    if(extension == "html") return "text/html";
+    if(extension == "css") return "text/css";
+    if(extension == "js") return "text/javascript";
+    if(extension == "jpg" || extension == "jpeg") return "image/jpeg";
+    if(extension == "png") return "image/png";
+    if(extension == "json") return "application/json";
+}
+
 const servidor = createServer((peticion, respuesta) => {
     respuesta.writeHead(200, {"Content-type" : "text/html"});
     respuesta.write("algo");
